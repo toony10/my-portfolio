e016dca3b31ae0f77eca3d8321516a8e9bb170bf
toony10 <amiranton257@gmail.com>
2023-05-09 00:12:20 +0300

    lol2

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,6 +2,7 @@ import "./intro.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
 import { BsDownload } from "react-icons/bs";
+// import { introImg } from "assets/";
 export default function Intro() {
   useEffect(() => {
     init(textRef.current, {
@@ -18,7 +19,7 @@ export default function Intro() {
       <div className='left'>
         <div className='imgContainer'>
           <img
-            src='assets/imgs/man.png'
+            src='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blta2b39c95d6288a7b/6415b161e016021836114349/messi.jpg?auto=webp&format=pjpg&width=1080&quality=60'
             style={{ objectFit: "cover" }}
             alt=''
           />
